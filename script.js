bf55f26bafa69af5075330542bd4a8e2ef6bef72
mremoto <mremotoo@gmail.com>
2022-03-13 07:58:08 +0000

    Fix bug(console logging returns unexpected results or undefined in script.js

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
-function computerPlay (){
-    let randomAnswer = ["rock", "paper", "scissors"];
-    return randomAnswer[Math.floor(Math.random() * 3)];
+
+function playerPlay(){
+    let playerAns = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    return(playerAns);
 }
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection === "rock"){
-        if(computerSelection === "rock"){
-            return("Draw!")
-        }else if(computerSelection === "paper"){
-            return("Lose!")
+function computerPlay(){
+    let randomAnswer = ['rock', 'paper', 'scissors'];
+    return(randomAnswer[Math.floor(Math.random() * 3)]);
+};
+
+function playRound(x, y){
+   
+    if(x === "rock"){
+        if(y === "rock"){
+            return("Draw!");
+        }else if(y === "paper"){
+            return("Lose!");
         }else{
-            return("Win!")
+            return("Win!");
         }
     }
 
-    if(playerSelection === "paper"){
-        if(computerSelection === "rock"){
-            return("Win!")
-        }else if(computerSelection === "paper"){
-            return("Draw!")
+    if(x === "paper"){
+        if(y === "rock"){
+            return("Win!");
+        }else if(y === "paper"){
+            return("Draw!");
         }else{
-            return("Lose!")
+            return("Lose!");
         }
     }
 
-    if(playerSelection === "scissors"){
-        if(computerSelection === "rock"){
-            return("Lose!")
-        }else if(computerSelection === "paper"){
-            return("Win!")
+    if(x === "scissors"){
+        if(y === "rock"){
+            return("Lose!");
+        }else if(y === "paper"){
+            return("Win!");
         }else{
-            return("Draw!")
+            return("Draw!");
         }
     }
 }
 
-function game(){
-    
-    for(let i = 0; 1 < 5; i++){
-        let computerScore = 0;
-        let playerScore = 0;
-        
-    
-    }
-    //return winner
-}
-
-const playerSelection = "paper";
-const computerSelection = computerPlay();
+console.log(playRound(playerPlay(), computerPlay()));
